fb94a3101292641bbe0d0295d929389a1bc5b17c
Arjan de Ruijter <rra@mboutrecht.nl>
2016-09-19 11:47:28 +0200

    get var id

--- showName.php
+++ showName.php
@@ -145,7 +145,7 @@
                         <td>".$record['firstname']."</td>
                         <td>".$record['infix']."</td>
                         <td>".$record['lastname']."</td> 
-                        <td><a href='remove.php'><img src='images/drop.png' alt='kruis'></a></td>          
+                        <td><a href='remove.php?id=".$record['id']."'><img src='images/drop.png' alt='kruis'></a></td>          
                   <tr>";
      } 
      echo "</table>";
